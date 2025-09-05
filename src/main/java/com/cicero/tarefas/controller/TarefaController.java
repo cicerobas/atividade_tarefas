416b7a7a6cbed44e491185d7c5806b66c9936082
Cicero B. Simões <cbruno.dev@gmail.com>
2025-09-05 10:38:56 -0300

    estrutura

---
+++ src/main/java/com/cicero/tarefas/controller/TarefaController.java
@@ -0,0 +1,10 @@
+package com.cicero.tarefas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tarefas")
+public class TarefaController {
+
+}
---
+++ src/main/java/com/cicero/tarefas/model/Tarefa.java
@@ -0,0 +1,61 @@
+package com.cicero.tarefas.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tarefas")
+public class Tarefa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "titulo_tarefa")
+    private String nome;
+
+    @Column(name = "data_entrega")
+    private LocalDate dataEntrega;
+
+    @Column(name = "nome_responsavel")
+    private String responsavel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataEntrega() {
+        return dataEntrega;
+    }
+
+    public void setDataEntrega(LocalDate dataEntrega) {
+        this.dataEntrega = dataEntrega;
+    }
+
+    public String getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(String responsavel) {
+        this.responsavel = responsavel;
+    }
+
+    
+}
